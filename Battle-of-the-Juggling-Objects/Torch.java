149e3aa4547d7c5561ff2428c5eb40400d47e4f6
David English <david@operaonfilm.com>
2014-11-29 15:47:27 -0800

    Added documentation for the torch and ball classes

--- Battle-of-the-Juggling-Objects/Torch.java
+++ Battle-of-the-Juggling-Objects/Torch.java
@@ -8,13 +8,32 @@
 public class Torch extends GamePiece
 {
     /**
-     * Constructor for objects of class Torch
+     * Default Torch Constructor
+     *
+     */
+    public Torch()
+    {
+        super("", null);
+    }
+    
+    /**
+     * Torch Constructor
+     *
+     * @param newPieceDisplay a string to represent the look of the piece
+     * @param newOwningPlayer the player who owns the particular piece
      */
     public Torch(String newPieceDisplay, Player newOwningPlayer)
     {
         super(newPieceDisplay, newOwningPlayer);
     }
 
+    /**
+     * Method isLegalMove
+     *
+     * @param from the origin location
+     * @param to the destination location
+     * @return a boolean value, true if the move is legal for that piece or false if it is not
+     */
     public boolean isLegalMove(Location from, Location to)
     {
         if(from.getXPosition() != to.getXPosition()){
