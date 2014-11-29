b9ebcbf64a79319c9a43fb91a2bc9ec1fb82a75c
David English <david@operaonfilm.com>
2014-11-29 15:51:57 -0800

    Added documentation for the location class

--- Battle-of-the-Juggling-Objects/Location.java
+++ Battle-of-the-Juggling-Objects/Location.java
@@ -11,7 +11,8 @@ public class Location
     private int yPosition;
 
     /**
-     * Constructor for objects of class Location
+     * Default Location Constructor
+     *
      */
     public Location()
     {
@@ -19,12 +20,23 @@ public class Location
         setYPosition(0);
     }
     
+    /**
+     * Location Constructor
+     *
+     * @param newXPosition a horizontal position
+     * @param newYPosition a vertical position
+     */
     public Location(int newXPosition, int newYPosition)
     {
         setXPosition(newXPosition);
         setYPosition(newYPosition);
     }
 
+    /**
+     * Method setXPosition
+     *
+     * @param newXPosition a horizontal position
+     */
     public void setXPosition(int newXPosition)
     {
         if(newXPosition > 0){
@@ -32,6 +44,11 @@ public class Location
         }
     }
        
+    /**
+     * Method setYPosition
+     *
+     * @param newYPosition a vertical position
+     */
     public void setYPosition(int newYPosition)
     {
         if(newYPosition > 0){
@@ -39,16 +56,31 @@ public class Location
         }
     }
        
+    /**
+     * Method getXPosition
+     *
+     * @return the horizontal position
+     */
     public int getXPosition()
     {
         return xPosition;
     }
     
+    /**
+     * Method getYPosition
+     *
+     * @return the vertical position
+     */
     public int getYPosition()
     {
         return yPosition;
     }
     
+    /**
+     * Method toString
+     *
+     * @return a string comprised of the horizontal and vertical positions
+     */
     public String toString()
     {
         return "" + getXPosition() + getYPosition();
