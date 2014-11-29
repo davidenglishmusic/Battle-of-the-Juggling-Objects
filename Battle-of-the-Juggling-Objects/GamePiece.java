442218d18890b243aad60197c7a959ffb13df27b
David English <david@operaonfilm.com>
2014-11-29 15:41:50 -0800

    Added documentation for the GamePiece abstract class

--- Battle-of-the-Juggling-Objects/GamePiece.java
+++ Battle-of-the-Juggling-Objects/GamePiece.java
@@ -10,18 +10,33 @@ public abstract class GamePiece
     private String pieceDisplay;
     private Player owningPlayer;
 
+    /**
+     * Default GamePiece Constructor
+     *
+     */
     public GamePiece()
     {
         setPieceDisplay("");
         setOwningPlayer(null);
     }
 
+    /**
+     * GamePiece Constructor
+     *
+     * @param newPieceDisplay a string to represent the look of the piece
+     * @param newOwningPlayer the player who owns the particular piece
+     */
     public GamePiece(String newPieceDisplay, Player newOwningPlayer)
     {
         setPieceDisplay(newPieceDisplay);
         setOwningPlayer(newOwningPlayer);
     }
     
+    /**
+     * Method setPieceDisplay
+     *
+     * @param newPieceDisplay a string to represent the look of the piece
+     */
     public void setPieceDisplay(String newPieceDisplay)
     {
         if(newPieceDisplay != null && newPieceDisplay.length() == 1)
@@ -30,6 +45,11 @@ public abstract class GamePiece
         }
     }
     
+    /**
+     * Method setOwningPlayer
+     *
+     * @param newOwningPlayer the player who owns the particular piece
+     */
     public void setOwningPlayer(Player newOwningPlayer)
     {
         if(newOwningPlayer != null){
@@ -37,20 +57,42 @@ public abstract class GamePiece
         }
     }
     
+    /**
+     * Method getPieceDisplay
+     *
+     * @return the display of the piece
+     */
     public String getPieceDisplay()
     {
         return pieceDisplay;
     }
     
+    /**
+     * Method getOwningPlayer
+     *
+     * @return the owning player
+     */
     public Player getOwningPlayer()
     {
         return owningPlayer;
     }
     
+    /**
+     * Method toString
+     *
+     * @return the display of the piece
+     */
     public String toString()
     {
         return getPieceDisplay();
     }
     
+    /**
+     * Method isLegalMove
+     *
+     * @param from the origin location
+     * @param to the destination location
+     * @return a boolean value, true if the move is legal for that piece or false if it is not
+     */
     abstract public boolean isLegalMove(Location from, Location to);
 }
