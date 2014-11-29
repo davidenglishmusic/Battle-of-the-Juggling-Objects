1938f309686d27c760f88271bdd8811ff4b5c655
David English <david@operaonfilm.com>
2014-11-29 15:38:18 -0800

    Added documentation for the player class

--- Battle-of-the-Juggling-Objects/Player.java
+++ Battle-of-the-Juggling-Objects/Player.java
@@ -9,16 +9,30 @@ public class Player
 {
     private int playerNumber;
 
+    /**
+     * Default Player Constructor
+     *
+     */
     public Player()
     {
         setPlayerNumber(0);
     }
     
+    /**
+     * Player Constructor
+     *
+     * @param newPlayerNumber the new player number // Ultimately it might be nice to get some way of generating this number based on a class variable
+     */
     public Player(int newPlayerNumber)
     {
         setPlayerNumber(newPlayerNumber);
     }
 
+    /**
+     * Method setPlayerNumber
+     *
+     * @param newPlayerNumber the new player number
+     */
     public void setPlayerNumber(int newPlayerNumber)
     {
         if(newPlayerNumber > 0){
@@ -26,11 +40,21 @@ public class Player
         }
     }
     
+    /**
+     * Method getPlayerNumber
+     *
+     * @return the player number
+     */
     public int getPlayerNumber()
     {
         return playerNumber;
     }
     
+    /**
+     * Method toString
+     *
+     * @return a string containing the word player and their particular player number
+     */
     public String toString()
     {
         return "Player " + getPlayerNumber();
